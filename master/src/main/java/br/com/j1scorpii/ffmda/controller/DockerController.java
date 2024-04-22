73b057f79b8bdda9bbcaa712d75fb5d8d19ef97f
icemagno <magno.mabreu@gmail.com>
2024-04-22 20:33:06 -0300

    Remove script.js from component pages

--- master/src/main/java/br/com/j1scorpii/ffmda/controller/DockerController.java
+++ master/src/main/java/br/com/j1scorpii/ffmda/controller/DockerController.java
@@ -1,6 +1,5 @@
 package br.com.j1scorpii.ffmda.controller;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
