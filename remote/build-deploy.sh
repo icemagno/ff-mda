785321d4510b3c1f15ae4ae89b461b789e7afa06
icemagno <magno.mabreu@gmail.com>
2024-04-26 17:18:03 -0300

    First steps on the remote Agent

--- remote/build-deploy.sh
+++ remote/build-deploy.sh
@@ -16,7 +16,7 @@ docker run --name ffmda-agent --network=ffmda --hostname=ffmda-agent \
 --restart=always \
 -d -p 36780:8080 \
 -e NODE_NAME=FF-N1 \
--e ORG_NAME=The New Org \
+-e ORG_NAME="The New Org" \
 -e HOST_NAME=firefly.s1 \
 -e HOST_ADDRESS=192.168.0.205 \
 -v /var/run/docker.sock:/var/run/docker.sock \
