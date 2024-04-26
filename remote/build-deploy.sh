fdb03bc4ba71b8514935b46831ce514c5f50c599
icemagno <magno.mabreu@gmail.com>
2024-04-26 17:11:20 -0300

    First steps on the remote Agent

--- remote/build-deploy.sh
+++ remote/build-deploy.sh
@@ -6,24 +6,22 @@ mvn clean package
 docker network create ffmda
 
 mkdir /srv/ffmda
-cp manifest.json /srv/ffmda
 
-docker rmi magnoabreu/ffmda-master:0.1
-docker build --tag=magnoabreu/ffmda-master:0.1 --rm=true .
+docker rmi magnoabreu/ffmda-agent:0.1
+docker build --tag=magnoabreu/ffmda-agent:0.1 --rm=true .
 
-docker stop ffmda-master && docker rm ffmda-master
+docker stop ffmda-agent && docker rm ffmda-agent
 
-# For IPFS
-sysctl -w net.core.rmem_max=2500000
-sysctl -w net.core.wmem_max=2500000
-
-
-docker run --name ffmda-master --network=ffmda --hostname=ffmda-master \
+docker run --name ffmda-agent --network=ffmda --hostname=ffmda-agent \
 --restart=always \
 -d -p 36780:8080 \
+-e NODE_NAME=FF-N1 \
+-e ORG_NAME=The New Org \
+-e HOST_NAME=firefly.s1 \
+-e HOST_ADDRESS=192.168.0.205 \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /srv:/srv \
-magnoabreu/ffmda-master:0.1
+magnoabreu/ffmda-agent:0.1
 
-# docker push magnoabreu/ffmda-master:0.1
+# docker push magnoabreu/ffmda-agent:0.1
 
