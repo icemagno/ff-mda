62d2e443f113fa70c4d6420b551a6cb6a1e3be47
icemagno <magno.mabreu@gmail.com>
2024-04-25 21:51:54 -0300

    Merge some common JS functions in one file

--- master/src/main/resources/static/common/components/dataexchange.js
+++ master/src/main/resources/static/common/components/dataexchange.js
@@ -50,11 +50,7 @@ $( document ).ready(function() {
 		});
 	});	
 
-	$("#removeCont").click( ()=>{
-		if( isDisabled( "#removeCont" ) ) return;
-		log('Not Implemented');
-	});
-		
+	
 	$("#restartCont").click( ()=>{
 		if( isDisabled( "#restartCont" ) ) return;
 		log("Wait...")
@@ -94,50 +90,6 @@ $( document ).ready(function() {
 	});	
 	 
 });
-
-function updateFixedLog( what ){
-	$("#containerLogFixed").html( '<p style="margin:0px;padding:0px">' + what + '</p>' );
-}
-
-function log( what ){
-	if( lastPullMessage == what ) return;
-	lastPullMessage = what;
-	if( $("#containerLog p").length > 20 ) $("#containerLog p").first().remove();
-	$("#containerLog").append( '<p style="margin:0px;padding:0px">' + what + '</p>' );
-}
-
-function isDisabled( who ){
-	return $(who).hasClass( "disabled" )	
-}
-
-function enableBtn( btn ){
-	$( btn ).removeClass('disabled');
-	$( btn ).removeClass('btn-default');
-	$( btn ).addClass('btn-primary');
-}
-
-function disableBtn( btn ){
-	$( btn ).addClass('disabled');
-	$( btn ).addClass('btn-default');
-	$( btn ).removeClass('btn-primary');
-}
-
-function setButtons( what ){
-	
-	if( what == 'play' ){
-		enableBtn("#startCont");
-		disableBtn("#stopCont");
-		disableBtn("#restartCont");
-	}
-	
-	if( what == 'stop-restart' ){
-		disableBtn("#startCont");
-		enableBtn("#stopCont");
-		enableBtn("#restartCont");
-	}
-}
-
-
 
 function processConfig( config, prefix ){
 	for (var key in config) {
